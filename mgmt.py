609e8d1b4ca21a072dbe8c6c026b964b96b15bad
CasuallyDreamin <ysn.arambash@gmail.com>
2025-03-25 17:31:07 +0330

    Added get_all / get_by_max_price functionalities

--- mgmt.py
+++ mgmt.py
@@ -2,10 +2,12 @@ from dll import dll
 from hashtable import hashtable
 from shipment import shipment
 from tools import generate_ship_id
+from arr import arr
 
 class mgmt:
     def __init__(self):
-        #placeholder DS (should add array/hashtable for O(n) look up)
+        #hashtable for O(1) average-case and O(n) worst-case look up
+        #doubly linked list for O(n) show all items
         self.shipments_ht = hashtable()
         self.shipments_dll = dll()
 
@@ -18,6 +20,7 @@ class mgmt:
                 price,
                 category):
         
+        #generating unique shipment id if none given
         while ship_id == None or self.shipments_ht.get(ship_id) != None:
             ship_id = generate_ship_id(category)
 
@@ -28,4 +31,31 @@ class mgmt:
                  arrival_date,
                  price,
                  category)
+        
+        self.shipments_ht.insert(new_shipment.ship_id, new_shipment)
+
+    def get_shipment_by_id(self, shipment_id) -> shipment | None:
+        return self.shipments_ht.get(shipment_id)
+    
+    def get_all_shipments(self) -> arr:
+        return self.shipments_dll.get_all_arr()
+    
+    def get_all_by_max_price(self, max_price) -> arr | None:
+        curr_shipment = self.shipments_dll.head
+        target_shipments = dll()
+
+        if curr_shipment == None:
+            return None
+        
+        while curr_shipment != None:
+            if curr_shipment.data.price <= max_price:
+                target_shipments.add_first(curr_shipment.data)
+            curr_shipment = curr_shipment.next
+
+        return target_shipments.get_all_arr()
+
+        
+
+
+    
         
